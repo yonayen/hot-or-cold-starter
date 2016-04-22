be4961d2aed765f13107d87b972af4bdcae66a19
Yonathan Ayenew <yonayen@gmail.com>
2016-04-22 08:31:14 -0700

    add new game ability

--- js/app.js
+++ js/app.js
@@ -33,3 +33,99 @@ function getGuess() {
     });
 }
 
+getGuess();
+
+
+
+// Game Function
+function game() {
+    var guess = parseInt($('#guess').val());
+
+    if (guess !== null && $.isNumeric(guess) && (1 < guess < 101)) {
+        $('#guess').val('');
+        numberOfGuesses += 1;
+        guesses.push(guess);
+        distance = Math.abs(answer - guess);
+        previousDistance = Math.abs(answer - guesses[guesses.length - 2]);
+
+
+        // Get it correct
+        if (guess === answer) {
+            $('#hint').html('Congrats! You got it in ' + numberOfGuesses + ' attempts! The secret number was ' + answer);
+        } else {
+            console.log(guess, answer, previousDistance, distance);
+            if (isNaN(previousDistance)) {
+                if (guess > answer) {
+                    $('#hint').html('Guess lower! Last guess: ' + guess);
+                } else if (guess < answer) {
+                    $('#hint').html('Guess higher! Last guess: ' + guess);
+                }
+
+            } else if (distance > previousDistance) {
+                if (guess > answer) {
+                    $('#hint').html('You\'re getting colder, guess lower! Last guess: ' + guess);
+                } else if (guess < answer) {
+                    $('#hint').html('You\'re getting colder, guess higher! Last guess: ' + guess);
+                }
+            } else if (distance < previousDistance) {
+                if (guess > answer) {
+                    $('#hint').html('You\'re getting hotter, guess lower! Last guess: ' + guess);
+                } else if (guess < answer) {
+                    $('#hint').html('You\'re getting hotter, guess higher! Last guess: ' + guess);
+                }
+            } else if (distance === previousDistance) {
+                if (guess > answer) {
+                    $('#hint').html('You\'re on fire, guess lower! Last guess: ' + guess);
+                } else if (guess < answer) {
+                    $('#hint').html('You\'re on fire, guess higher! Last guess: ' + guess);
+                }
+            } else {
+                $('#hint').html('ERROR: Your guess must be a number between 1 and 100').css({
+                    color: 'red'
+                });
+            }
+        }
+    }
+
+    	 // Start a new game
+	    $('#newgame').click(function (e) {
+	        e.preventDefault();
+	        answer = Math.floor((Math.random() * 100) + 1);
+	        console.log(answer);
+	        numberOfGuesses = 0;
+	        guesses = [];
+	        distance = null;
+	        previousDistance = null;
+	        $('#hint').html('');
+	        $('#guess').val('');
+	    });
+
+
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
