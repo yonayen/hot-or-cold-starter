c13d50d150e12975b6d39b519778acade2d47c25
Yonathan Ayenew <yonayen@gmail.com>
2016-04-22 08:16:39 -0700

    function getGuess()

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-
 $(document).ready(function(){
-
-
-
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -17,4 +13,23 @@ $(document).ready(function(){
 
 });
 
+
+//Initial Variables
+
+var answer = Math.floor((Math.random() * 100) + 1);
+console.log("The secret number is: " + answer);
+var numberOfGuesses = 0;
+var guesses = [];
+guesses.length = 0;
+var distance = null;
+var previousDistance = null;
+
+function getGuess() {
+    $("#submit").click(game);
+    $("#guess").keydown(function (enter) {
+        if (enter.keyCode == 13) {
+            game();
+        }
+    });
+}
 
